d4668258518109d2efee055310d36833cc88aa03
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2021-09-13 01:04:38 +0800

    Login : Add psw expressions

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,51 +1,104 @@
 <template>
   <div>
-    <p>Choose Account Type</p>
+    <section class="l-title">
+      <h2 class="o-title">Choose Account Type</h2>
+    </section>
     <!-- 身分選擇 -->
-    <ul class="l-identityCard">
-      <li
+    <section class="l-identityCard">
+      <div
         class="l-identityCard__item"
         v-for="(person, idx) in identities"
         :key="idx"
       >
-        <IdentityCard :identity="person"></IdentityCard>
-      </li>
-    </ul>
+        <identity-card :identity="person" :idx="idx"></identity-card>
+      </div>
+    </section>
     <!-- 招呼語 -->
-    <div>
-      <p>Hello Doctor</p>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </div>
-    <!-- 登入欄位 -->
-    <div>
-      <input type="email" placeholder="EMAIL" />
-      <input type="text" />
-    </div>
-    <div>
-      <button class="o-btn">Login</button>
-    </div>
+    <section class="l-title">
+      <div class="o-text">
+        <p>Hello !</p>
+        <p>Please full out the form below to get started</p>
+      </div>
+    </section>
+    <!-- 登入 -->
+    <section class="l-form">
+      <div class="l-form__row">
+        <div class="c-icobox c-icobox--email">
+          <i class="c-icobox c-icobox__ico"></i>
+          <input
+            class="c-icobox c-icobox__inp"
+            type="text"
+            placeholder="Username"
+            v-model="acc.username"
+          />
+          <label class="c-icobox c-icobox__lbl" for="">Username</label>
+        </div>
+      </div>
+      <div class="l-form__row">
+        <div class="c-icobox c-icobox--lock">
+          <i class="c-icobox c-icobox__ico"></i>
+          <input
+            class="c-icobox c-icobox__inp"
+            type="password"
+            placeholder="Password"
+            v-model="acc.password"
+          />
+          <label class="c-icobox c-icobox__lbl" for="">Password</label>
+        </div>
+      </div>
+      <!-- 按鈕 -->
+      <div class="l-form__btn">
+        <div class="l-link">
+          <p>No account ?</p>
+          <a href="#" class="o-link">&nbsp;Sign up</a>
+        </div>
+        <button class="o-btn" @click="checking()">Login</button>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import IdentityCard from "@/components/IdentityCard.vue";
+import identityCard from "@/components/IdentityCard.vue";
+
 export default {
   components: {
-    IdentityCard,
+    identityCard,
   },
-  props: ["identity"],
+  props: ["identity", "idx"],
   data() {
     return {
-      identities: [
-        "Doctor",
-        "Patient",
-        "Doctor",
-        "Doctor",
-        "Doctor",
-        "Doctor",
-        "Doctor",
-      ],
+      identities: ["Doctor", "Patient"],
+      acc: {
+        username: "",
+        password: "",
+      },
     };
+  },
+  methods: {
+    checking() {
+      let nameArr = this.acc.username.split("");
+      let nameStr = this.acc.username;
+      let pswArr = this.acc.password.split("");
+      let pswStr = this.acc.password;
+
+      if (nameArr.length < 6 || pswArr.length < 6) {
+        alert("帳號或密碼至少需 6 字元");
+        return;
+      }
+
+      for (let i = 0; i < pswArr.length; i++) {
+        let str = pswStr.slice(i, i + 6);
+        // console.log(str);
+        if (nameStr.includes(str)) {
+          alert("密碼不可與帳號任意 6 碼重複");
+          return;
+        } else {
+          alert("登入成功");
+          return;
+        }
+      }
+    },
   },
 };
 </script>
