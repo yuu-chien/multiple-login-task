2aa31074499934ed1895cfe45ed8f3926bc26e47
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2021-09-13 01:02:56 +0800

    Component : Edit identityCard to radio type

--- src/components/IdentityCard.vue
+++ src/components/IdentityCard.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="c-identityCard is-check">
+  <!-- <div class="c-identityCard is-check">
     <div class="c-identityCard__kv">
       <img :src="require(`@/assets/image/${identity}_3x.svg`)" />
     </div>
     <div class="c-identityCard__tit">{{ identity }}</div>
+  </div> -->
+  <div class="c-identityCard">
+    <input
+      class="c-identityCard__inp"
+      type="radio"
+      name="identity_type"
+      :id="'identity_' + idx"
+    />
+    <label class="c-identityCard__lbl" :for="'identity_' + idx">
+      <div class="c-identityCard__kv">
+        <img :src="require(`@/assets/image/${identity}_3x.svg`)" />
+      </div>
+      <div class="c-identityCard__tit">{{ identity }}</div>
+    </label>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["identity"],
+  props: ["identity", "idx"],
   data() {
     return {};
   },
